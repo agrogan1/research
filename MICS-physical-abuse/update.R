1e8119e77e570e086c27fc32ad28452e17ff8071
Andrew Grogan-Kaylor <agrogan@0587285531.wireless.umich.net>
2022-03-03 12:35:48 -0500

    higher density video

--- MICS-physical-abuse/update.R
+++ MICS-physical-abuse/update.R
@@ -5,6 +5,7 @@ library(xaringanBuilder)
 build_pdf("./MICS-physical-abuse/MICS-physical-abuse.Rmd")
 
 build_mp4("./MICS-physical-abuse/MICS-physical-abuse.Rmd",
+          density = 300,
           fps = .25)
 
 # xaringan::moon_reader("./MICS-physical-abuse/MICS-physical-abuse.Rmd")
