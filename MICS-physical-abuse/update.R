72363cf9db7bc56bdcc6119ec5a69a8b7b676bdd
Grogan-Kaylor <agrogan@UMICH.EDU>
2022-03-23 10:23:41 -0400

    2 column layout

--- MICS-physical-abuse/update.R
+++ MICS-physical-abuse/update.R
@@ -4,8 +4,8 @@ library(xaringanBuilder)
 
 build_pdf("./MICS-physical-abuse/MICS-physical-abuse.Rmd")
 
-build_mp4("./MICS-physical-abuse/MICS-physical-abuse.Rmd",
-          density = 300,
-          fps = .25)
+# build_mp4("./MICS-physical-abuse/MICS-physical-abuse.Rmd",
+#           density = 300,
+#           fps = .25)
 
 # xaringan::moon_reader("./MICS-physical-abuse/MICS-physical-abuse.Rmd")
